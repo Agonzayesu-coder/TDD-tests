dbb92e4df8269c4665f7ff1a4ced96983054b729
Agonzayesu <agonzayesu@gmail.com>
2025-07-06 23:51:00 +0300

    Green, passed

--- Test/test.js
+++ Test/test.js
@@ -9,7 +9,7 @@
 const assertEqual = require('assert');  
 const { multiply } = require('../multiply'); 
 describe('multiply', () => {
-  it('Should return 1 when multiplying 1 and 1', () => {
+  it('Should return 4 when multiplying 2 and 2', () => {
     assertEqual(multiply(2, 2),4);  
   });
 });
