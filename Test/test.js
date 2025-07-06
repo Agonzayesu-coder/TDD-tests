5dcda25a0e4f6dc89b6e17ecab834d51526ea38d
Agonzayesu <agonzayesu@gmail.com>
2025-07-06 23:49:33 +0300

    Red, multiply is not a function

--- Test/test.js
+++ Test/test.js
@@ -1,7 +1,15 @@
+// const assertEqual = require('assert');  
+// const { multiply } = require('../multiply'); 
+// describe('multiply', () => {
+//   it('Should return 1 when multiplying 1 and 1', () => {
+//     assertEqual(multiply(1, 1), 1);  
+//   });
+// });
+
 const assertEqual = require('assert');  
 const { multiply } = require('../multiply'); 
 describe('multiply', () => {
   it('Should return 1 when multiplying 1 and 1', () => {
-    assertEqual(multiply(1, 1), 1);  
+    assertEqual(multiply(2, 2),4);  
   });
 });
