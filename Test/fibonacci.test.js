2518a678ab4d53c3f909bf6b0bb9c5c01d78ce45
Agonzayesu <aagonzayesu@student.refactory.academy>
2025-07-09 20:45:44 +0300

    second test passed

--- Test/fibonacci.test.js
+++ Test/fibonacci.test.js
@@ -5,17 +5,20 @@ const assert = require('assert');
 const { fibonacci } = require('./fibonacci');
 
 //fibonacci(n), n is the 'n'th number in the Fibonacci sequence
-describe('Fibonacci', () => {
   // First test cycle
-  it('fibonacci(0) = 0', () => {
-    assert.equal(fibonacci(0), 0);
+ 
+  describe('Fibonacci', () => {
+      it('fibonacci(0) = 0', () => {
+        assert.equal(fibonacci(0), 0);
+ });
 
     //second test cycle
-    it('fibonacci(1) = 1', () => {
-      assert.equal(fibonacci(1), 1);
-    });
+        it('fibonacci(1) = 1', () => {
+            assert.equal(fibonacci(1), 1);
+    });    
+});
 
     // Third test cycle
-  });
+ 
 
-  });
+  
