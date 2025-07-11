93be3ccfc730fca31c06ea9ac0602f5629d22619
Agonzayesu <aagonzayesu@student.refactory.academy>
2025-07-11 19:00:41 +0300

    all green, 6/6 tests passed

--- test/PasswordValidator.py
+++ test/PasswordValidator.py
@@ -3,8 +3,13 @@ import re
 def is_valid_password(password):
     if len(password) < 8:
         return False
-    if not re.search(r'[A-Z]', password):
+    if not re.search(r'[A-Z]', password): #check for uppercase letter
         return False
-   
+    if not re.search(r'[a-z]', password): #check for lowercase letter
+        return False
+    if not re.search(r'\d', password):    #check for digit
+        return False
+    if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password): #check for special character
+        return False
     return True
     
